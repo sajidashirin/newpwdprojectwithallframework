7f516e1774568842c73a20a8a694af3a329cdcf7
Sajida <sjdshrn956@gmail.com>
2026-05-21 14:26:59 +0300

    Updated login button in feature branch

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -8,7 +8,8 @@ class LoginPage {
 
         this.username = '#user-name';
         this.password = '#password';
-        this.loginButton = '#login-button';
+        //this.loginButton = '#login-button';
+        this.loginButton = '#feature-login-btn';
         this.errorMessage = 'h3[data-test="error"]';
     }
 
